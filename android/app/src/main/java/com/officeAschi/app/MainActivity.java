cf299c0b1a23ad9e6802b88eba4832e919b3107a
ARIJIT KUNDU <arijit.kundu@hyland.com>
2026-05-11 09:56:50 +0530

    chore: update Capacitor dependencies to version 8.3.3 in package.json

---
+++ android/app/src/main/java/com/officeAschi/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.officeAschi.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.officeAschi.app',
+  appName: 'Office Aschi',
+  webDir: 'dist/office-aschi/browser'
+};
+
+export default config;
